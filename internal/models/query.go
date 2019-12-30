4abcbe263f9b1f1851a91e7063456f54747d1e47
Jesse Millar <jesse.millar@microsoft.com>
2019-12-30 14:04:41 -0600

    Getting the JSON structure finalized
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- internal/models/query.go
+++ internal/models/query.go
@@ -1,9 +1,75 @@
 package models
 
+import "github.com/jessemillar/sabacc/internal/deck"
+
+/*
+JSON sample:
+
+{
+	"round": 1,
+  "turn": 1,
+  "players": [
+    {
+      "email": "hellojessemillar@gmail.com",
+      "hand": [
+        {
+          "value": 1,
+          "stave": "triangle"
+        },
+        {
+          "value": -1,
+          "stave": "circle"
+        }
+      ]
+    },
+    {
+      "email": "fairdinkumriley@gmail.com",
+      "hand": [
+        {
+          "value": 1,
+          "stave": "triangle"
+        },
+        {
+          "value": -1,
+          "stave": "circle"
+        }
+      ]
+    },
+    {
+      "email": "redscooterboy@gmail.com",
+      "hand": [
+        {
+          "value": 1,
+          "stave": "triangle"
+        },
+        {
+          "value": -1,
+          "stave": "circle"
+        }
+      ]
+    }
+  ],
+  "discards": [
+    {
+      "value": 1,
+      "stave": "triangle"
+    },
+    {
+      "value": -1,
+      "stave": "circle"
+    }
+  ]
+}
+*/
+
 type Query struct {
-	Round       int      `json:"round"`
-	Turn        int      `json:"turn"`
-	AllPlayers  []string `json:"players"`
-	AllHands    []string `json:"hands"`
-	AllDiscards []string `json:"discards"`
+	Round       int       `json:"round"`
+	Turn        int       `json:"turn"`
+	AllPlayers  []Player  `json:"players"`
+	AllDiscards deck.Deck `json:"discards"`
+}
+
+type Player struct {
+	Email string    `json:"email"`
+	Hand  deck.Deck `json:"hand"`
 }
