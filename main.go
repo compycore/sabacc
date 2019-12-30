b9522b2326d2065a066b9ce819b9133cfd42148b
Jesse Millar <jesse.millar@microsoft.com>
2019-12-30 16:50:50 +0000

    Cleaning up email generation and starting on deck creation
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,19 +9,36 @@ import (
 )
 
 func main() {
-	from := mail.NewEmail("Example User", "test@example.com")
-	subject := "Sending with Twilio SendGrid is Fun"
+	// TODO Move to an email package and allow for params
+	log.Println("Composing email")
+
+	from := mail.NewEmail("Sabaac Dealer", "sabaac@jessemillar.com")
+	subject := "Your Sabacc Game"
 	to := mail.NewEmail("Sabacc Player", "hellojessemillar@gmail.com")
-	plainTextContent := "and easy to do anywhere, even with Go"
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+
+	plainTextContent := "Click here to take your turn."
+	htmlContent := `It's hellojessemillar@gmail.com's turn!
+<br>
+<br>
+<a href="https://jessemillar.github.io/sabacc?player=test@test.com">Click here if that's you!</a>`
+
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+
 	response, err := client.Send(message)
 	if err != nil {
 		log.Println(err)
-	} else {
-		fmt.Println(response.StatusCode)
-		fmt.Println(response.Body)
-		fmt.Println(response.Headers)
 	}
+
+	// Catch issues with the email API
+	if response.StatusCode != 202 {
+		log.Println("There was an issue sending the email message:")
+		log.Println(response.StatusCode)
+		log.Println(response.Headers)
+		log.Println(response.Body)
+		os.Exit(1)
+	}
+
+	log.Println("Email sent")
 }
