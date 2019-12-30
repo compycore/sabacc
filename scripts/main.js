ba031eddb044bb3d8be23441f64316ce101abe17
Jesse Millar <jesse.millar@microsoft.com>
2019-12-30 15:28:53 -0600

    The UI can now retrieve and decode the URI query params
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
-var pickAction = "Pick your action!"
+// TODO Calculate your hand's value
+// TODO Read URI params
+// TODO Show game start/setup page if no query param info
+
+var database;
+var pickAction = "Pick your action!";
 
 function init() {
+	decodeParams();
+  checkTurn();
+}
+
+function decodeParams() {
+  database = decodeURIComponent(window.location.search.substr(1));
+	console.log(database);
+}
+
+function checkTurn() {
   swal({
       title: "Is it your turn?",
       text: "It's hellojessemillar@gmail.com's turn. Are you hellojessemillar@gmail.com?",
@@ -20,6 +35,43 @@ function init() {
     });
 }
 
+function gain() {
+  swal({
+      title: "Do you want to discard a card first?",
+      text: "You can discard a card before drawing a new one if you'd like.",
+      icon: "warning",
+      buttons: ["Nah.", "Yeah!"],
+    })
+    .then((willDiscard) => {
+      if (willDiscard) {
+        swal("Please tap on the card in your hand you wish to discard.", {
+          icon: "info",
+        });
+      } else {
+        swal(pickAction);
+      }
+    });
+}
+
+function discard(card) {
+  swal({
+      title: "Discard this card?",
+      text: "You want to discard " + card.name + "?",
+      icon: "warning",
+      buttons: ["Nah.", "Yeah!"],
+    })
+    .then((willDiscard) => {
+      if (willDiscard) {
+        swal("Card discarded!").then(() => {
+          // TODO Discard the card and make an API call
+          endTurn();
+        });
+      } else {
+        swal(pickAction);
+      }
+    });
+}
+
 function stand() {
   swal({
       title: "You want to stand?",
@@ -27,12 +79,12 @@ function stand() {
       icon: "warning",
       buttons: ["Nah.", "Yeah!"],
     })
-    .then((willDelete) => {
-      if (willDelete) {
+    .then((willStand) => {
+      if (willStand) {
         swal("You chose to stand.", {
           icon: "success",
         }).then(() => {
-          turnOver()
+          endTurn()
         });
       } else {
         swal(pickAction);
@@ -62,7 +114,8 @@ function trash() {
     });
 }
 
-function turnOver() {
+function endTurn() {
+  // TODO Make an API call
   swal({
     title: "Turn over.",
     text: "Your turn is now over! Please wait for the next email.",
