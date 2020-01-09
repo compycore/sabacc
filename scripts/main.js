4b202834f784ef735a0a7b7eb9acedad3d0e96ec
Jesse Millar <jesse.millar@microsoft.com>
2020-01-09 13:33:41 +0000

    Starting on a rematch functionality
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // TODO Implement dice rolling when the current player is the dealer and just ended their turn
 // Configuration
-var backendEndpoint = "https://jessemillar-sabacc.herokuapp.com/sabacc"
+var backendEndpoint = "https://jessemillar-sabacc.herokuapp.com/sabacc";
 
 // Global variables
 var turnTaken = false;
@@ -10,8 +10,21 @@ var database = JSON.parse(decodeURIComponent(window.location.search.substr(1)));
 console.log(database);
 
 function init() {
-  // Play the game if there's a game going
-  if (database && database.players.length > 0) {
+  if (database && database.rematch.length > 0) {
+    for (var i = 0; i < database.rematch.length; i++) {
+      database.players.push({
+        email: database.rematch[i]
+      });
+    }
+
+    swal(
+      "A new game has started with " +
+        value.split(",").join(", ") +
+        ". The first player listed will now receive an email! You can now close this window."
+    );
+    endTurn(false);
+  } else if (database && database.players.length > 0) {
+    // Play the game if there's a game going
     populatePage();
     swal(pickAction);
   } else {
@@ -20,34 +33,40 @@ function init() {
 }
 
 function startNewGame() {
-  swal("Enter email addresses (separated by a comma) of the people you want to play with:", {
-      content: "input",
-    })
-    .then((value) => {
-      if (value.split(",").length > 1 && value.split(",").length <= 8) {
-        var emailAddresses = value.split(",");
-        database = {
-          players: []
-        };
+  swal(
+    "Enter email addresses (separated by a comma) of the people you want to play with:",
+    {
+      content: "input"
+    }
+  ).then(value => {
+    if (value.split(",").length > 1 && value.split(",").length <= 8) {
+      var emailAddresses = value.split(",");
+      database = {
+        players: []
+      };
 
-        for (var i = 0; i < emailAddresses.length; i++) {
-          database.players.push({
-            "email": emailAddresses[i]
-          });
-        }
-
-        swal("A new game has started with " + value.split(",").join(", ") + ". The first player listed will now receive an email! You can now close this window.");
-        endTurn(false);
-      } else if (value.split(",").length == 1) {
-        swal("Please enter more than one email address.").then(() => {
-          location.reload(false);
-        });
-      } else if (value.split(",").length > 8) {
-        swal("Please enter less than eight email addresses.").then(() => {
-          location.reload(false);
+      for (var i = 0; i < emailAddresses.length; i++) {
+        database.players.push({
+          email: emailAddresses[i]
         });
       }
-    });
+
+      swal(
+        "A new game has started with " +
+          value.split(",").join(", ") +
+          ". The first player listed will now receive an email! You can now close this window."
+      );
+      endTurn(false);
+    } else if (value.split(",").length == 1) {
+      swal("Please enter more than one email address.").then(() => {
+        location.reload(false);
+      });
+    } else if (value.split(",").length > 8) {
+      swal("Please enter less than eight email addresses.").then(() => {
+        location.reload(false);
+      });
+    }
+  });
 }
 
 // Don't populate the page before we know we're dealing with the right person
@@ -60,13 +79,15 @@ function populatePage() {
 }
 
 function populateRound() {
-  document.getElementById("actions-header").innerHTML = "Actions (Round: " + database.round + ")";
+  document.getElementById("actions-header").innerHTML =
+    "Actions (Round: " + database.round + ")";
 }
 
 function populateScore() {
   var player = database.players[database.turn];
 
-  document.getElementById("your-hand-header").innerHTML = "Your Hand (Score: " + player.score + ") [" + player.email + "]";
+  document.getElementById("your-hand-header").innerHTML =
+    "Your Hand (Score: " + player.score + ") [" + player.email + "]";
 }
 
 // Populate card image divs
@@ -76,12 +97,20 @@ function populateYourHand() {
   for (var i = 0; i < hand.length; i++) {
     var card = hand[i];
 
-    document.getElementById("your-hand-cards").innerHTML += "<div class='two columns'><img src='" + getCardFilename(card) + "' class='u-max-full-width' onclick='swap(" + JSON.stringify(card) + ")' style='cursor: pointer;' /></div>";
+    document.getElementById("your-hand-cards").innerHTML +=
+      "<div class='two columns'><img src='" +
+      getCardFilename(card) +
+      "' class='u-max-full-width' onclick='swap(" +
+      JSON.stringify(card) +
+      ")' style='cursor: pointer;' /></div>";
   }
 }
 
 function populateDiscardPile() {
-  document.getElementById("discard-pile").innerHTML += "<div class='two columns'><img src='" + getCardFilename(database.discards[database.discards.length - 1]) + "' class='u-max-full-width' onclick='promptSwap()' style='cursor: pointer;' /></div>";
+  document.getElementById("discard-pile").innerHTML +=
+    "<div class='two columns'><img src='" +
+    getCardFilename(database.discards[database.discards.length - 1]) +
+    "' class='u-max-full-width' onclick='promptSwap()' style='cursor: pointer;' /></div>";
 }
 
 function promptSwap() {
@@ -91,20 +120,23 @@ function promptSwap() {
 function populateEnemyHands() {
   for (var i = 0; i < database.players.length; i++) {
     if (database.players[i].email != database.players[database.turn].email) {
-      document.getElementById("container").innerHTML += '<div class="row">'
-      document.getElementById("container").innerHTML += '<div class="u-full-width">'
-      document.getElementById("container").innerHTML += '<h4>' + database.players[i].email + '\'s Hand</h4>'
-      document.getElementById("container").innerHTML += '</div>'
-      document.getElementById("container").innerHTML += '</div>'
-      document.getElementById("container").innerHTML += '<div class="row">'
+      document.getElementById("container").innerHTML += '<div class="row">';
+      document.getElementById("container").innerHTML +=
+        '<div class="u-full-width">';
+      document.getElementById("container").innerHTML +=
+        "<h4>" + database.players[i].email + "'s Hand</h4>";
+      document.getElementById("container").innerHTML += "</div>";
+      document.getElementById("container").innerHTML += "</div>";
+      document.getElementById("container").innerHTML += '<div class="row">';
 
       var hand = database.players[i].hand;
 
       for (var j = 0; j < hand.length; j++) {
-        document.getElementById("container").innerHTML += '<div class="two columns"><img src="images/cards/back.jpg" class="u-max-full-width" /></div>'
+        document.getElementById("container").innerHTML +=
+          '<div class="two columns"><img src="images/cards/back.jpg" class="u-max-full-width" /></div>';
       }
 
-      document.getElementById("container").innerHTML += '</div>'
+      document.getElementById("container").innerHTML += "</div>";
     }
   }
 }
@@ -114,17 +146,17 @@ function gain() {
     title: "Do you want to discard a card first?",
     text: "You can discard a card before drawing a new one if you'd like.",
     icon: "warning",
-    buttons: ["Nah.", "Yeah!"],
-  }).then((willDiscard) => {
+    buttons: ["Nah.", "Yeah!"]
+  }).then(willDiscard => {
     if (willDiscard) {
       swal("Please tap on the card in your hand you wish to discard.", {
-        icon: "info",
+        icon: "info"
       });
     } else {
       swal({
         title: "You drew...",
         text: "the " + getCardString(database.draw) + "!",
-        icon: getCardFilename(database.draw),
+        icon: getCardFilename(database.draw)
       }).then(() => {
         database.players[database.turn].hand.push(database.draw);
         delete database.draw;
@@ -138,7 +170,14 @@ function swap(card) {
   if (!turnTaken) {
     swal({
       title: "What do you want to do with this card?",
-      text: "Do you want to swap your " + getCardString(card) + " with the " + getCardString(database.discards[database.discards.length - 1]) + " that's on top of the discard pile? Or discard your " + getCardString(card) + " and blindly draw a new card from the deck?",
+      text:
+        "Do you want to swap your " +
+        getCardString(card) +
+        " with the " +
+        getCardString(database.discards[database.discards.length - 1]) +
+        " that's on top of the discard pile? Or discard your " +
+        getCardString(card) +
+        " and blindly draw a new card from the deck?",
       icon: "warning",
       buttons: {
         cancel: {
@@ -146,7 +185,7 @@ function swap(card) {
           value: "cancel",
           visible: true,
           className: "",
-          closeModal: true,
+          closeModal: true
         },
         gain: {
           text: "Discard and draw",
@@ -161,23 +200,27 @@ function swap(card) {
           visible: true,
           className: "",
           closeModal: true
-        },
+        }
       }
-    }).then((value) => {
+    }).then(value => {
       // Find the object for the card in question in the player's hand
-      var cardIndexInHand = database.players[database.turn].hand.findIndex(element => element.value == card.value && element.stave == card.stave)
+      var cardIndexInHand = database.players[database.turn].hand.findIndex(
+        element => element.value == card.value && element.stave == card.stave
+      );
 
-      if (value = "swap") {
+      if ((value = "swap")) {
         // Remove the card in question from the player's hand
         database.players[database.turn].hand.splice(cardIndexInHand, 1);
         // Put the top of the discard pile in the player's hand
-        database.players[database.turn].hand.push(database.discards[database.discards.length - 1]);
+        database.players[database.turn].hand.push(
+          database.discards[database.discards.length - 1]
+        );
         // Remove the card that was just added to the player's hand from the discard pile
         database.discards.splice(database.discards.length - 1, 1);
         // Put the card in the discard pile
         database.discards.push(card);
         endTurn();
-      } else if (value = "gain") {
+      } else if ((value = "gain")) {
         // Remove the card in question from the player's hand
         database.players[database.turn].hand.splice(cardIndexInHand, 1);
         // Put the draw card in the player's hand
@@ -197,10 +240,10 @@ function stand() {
     title: "You want to stand?",
     text: "You're sure you want to do nothing for your turn?",
     icon: "warning",
-    buttons: ["Nah.", "Yeah!"],
-  }).then((willStand) => {
+    buttons: ["Nah.", "Yeah!"]
+  }).then(willStand => {
     if (willStand) {
-      endTurn()
+      endTurn();
     } else {
       swal(pickAction);
     }
@@ -213,15 +256,15 @@ function trash() {
     text: "If you trash, you drop out of the game permanently.",
     icon: "warning",
     buttons: ["Nope!", "Yes."],
-    dangerMode: true,
-  }).then((willDelete) => {
+    dangerMode: true
+  }).then(willDelete => {
     if (willDelete) {
       database.players.splice(database.turn, 1);
       endTurn(false);
 
       swal("You've withdrawn from the game.", {
         icon: "success",
-        button: "'Til the Spire.",
+        button: "'Til the Spire."
       });
     } else {
       swal("You're still in the game!");
@@ -232,23 +275,20 @@ function trash() {
 function endTurn(showTurnOver = true) {
   // Make an API call to the backend with the updated database info
   $.ajax({
-      url: backendEndpoint + "?" + encodeURIComponent(JSON.stringify(database)),
-      crossDomain: true
-    })
-    .done(function(data) {
-      if (showTurnOver) {
-        swal({
-          title: "Turn over.",
-          text: "Your turn is now over! Please wait for the next email.",
-          icon: "success",
-          button: "Patience, young padawan.",
-        }).then(
-          wipePage()
-        );
-      } else {
-        wipePage();
-      }
-    });
+    url: backendEndpoint + "?" + encodeURIComponent(JSON.stringify(database)),
+    crossDomain: true
+  }).done(function(data) {
+    if (showTurnOver) {
+      swal({
+        title: "Turn over.",
+        text: "Your turn is now over! Please wait for the next email.",
+        icon: "success",
+        button: "Patience, young padawan."
+      }).then(wipePage());
+    } else {
+      wipePage();
+    }
+  });
 }
 
 function wipePage() {
@@ -273,5 +313,11 @@ function getCardFilename(card) {
 }
 
 function getCardString(card, separator = " ") {
-  return card.stave + separator + getCardColor(card.value) + separator + Math.abs(card.value);
+  return (
+    card.stave +
+    separator +
+    getCardColor(card.value) +
+    separator +
+    Math.abs(card.value)
+  );
 }
