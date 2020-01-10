6c1b73dc6f625ca999ef4f2b6123642e7cc3517f
Jesse Millar <jesse.millar@microsoft.com>
2020-01-10 02:45:02 +0000

    Generating a full card
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- images/dev/generate.sh
+++ images/dev/generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-
+convert shapes/black.png frame/frame.png frame/header.png frame/ring.png staves/zero.png numbers/0.png reality/gloss.png reality/grunge-1.png -composite test.png
