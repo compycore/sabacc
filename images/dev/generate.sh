8c47c5535b27da693ce00e4389eac167bb470824
Jesse Millar <jesse.millar@microsoft.com>
2020-01-10 03:18:41 +0000

    Attempting a different composite method
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- images/dev/generate.sh
+++ images/dev/generate.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-convert shapes/black.png frame/frame.png frame/header.png frame/ring.png staves/zero.png numbers/0.png reality/gloss.png reality/grunge-1.png -composite test.png
+convert shapes/black.png frame/frame.png -composite test.png
+convert test.png frame/header.png -composite test.png
+convert test.png frame/ring.png -composite test.png
+convert test.png staves/zero.png -composite test.png
+convert test.png numbers/0.png -composite test.png
+convert test.png reality/gloss.png -composite test.png
+convert test.png reality/grunge-1.png -composite test.png
